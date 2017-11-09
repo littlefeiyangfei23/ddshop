ddbaa162d0759e5f06676b7006cce93104a32b64
Administrator <2451961234@qq.com>
2017-11-09 21:58:07 +0800

    根据商品名称、价格进行联合排序处理

--- dd-common/src/main/java/com/qf/ddshop/common/dto/Order.java
+++ dd-common/src/main/java/com/qf/ddshop/common/dto/Order.java
@@ -1,5 +1,8 @@
 package com.qf.ddshop.common.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * easyui的datagrid排序通用类
  */
@@ -7,6 +10,22 @@ public class Order {
 
     private String sort;
     private String order;
+
+
+    //最终拿到  title,price  这种格式的数据。
+    public List<String> getOrderParams() {
+//        spilt用来分割，用逗号进行分割,Ctrl+Alt+V,补全
+        String[] sorts = this.sort.split(",");//title，price
+        String[] orders = this.order.split(",");//asc,desc
+        List<String> list = new ArrayList<String>();
+        for (int i=0;i<sorts.length;i++){
+            String temp = sorts[i] +" "+orders[i];//title asc;price desc
+            list.add(temp);//[title asc;price desc]
+        }
+        return list;
+    }
+
+
 
     public String getSort() {
         return sort;
